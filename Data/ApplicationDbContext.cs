0d54c0fd80de47f4a41bad6e987113bfb48df67f
Aniyes <KiddDiop@gmail.com>
2022-04-23 03:03:08 -0700

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using BlogApplication.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogApplication.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<BlogUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+
+        internal static Task<object> ToListAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Enums/ReadyStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogApplication.Enums
+{
+    public enum ReadyStatus
+    {
+
+        Incomplete,
+        ProductionReady,
+        PreviewReady,
+
+    }
+}
